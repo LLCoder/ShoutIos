1204b922f104751cc88c61d0eca2bc1da8b4ec92
longzs <sky5yao@aliyun.com>
2014-09-15 11:02:35 +0800

    mainviewcontroller

--- Shout/MainViewController.m
+++ Shout/MainViewController.m
@@ -9,6 +9,8 @@
 #import "MainViewController.h"
 #import "GetViewController.h"
 #import "VentViewController.h"
+#import <ShareSDK/ShareSDK.h>
+
 
 #define k_ViewControl_Counts    3
 
@@ -109,6 +111,33 @@
 -(void)clickShare:(UITapGestureRecognizer*)sender{
     
     NSLog(@"share");
+    NSString *imagePath = [[NSBundle mainBundle] pathForResource:@"ShareSDK"  ofType:@"jpg"];
+    
+    //构造分享内容
+    id<ISSContent> publishContent = [ShareSDK content:@"分享内容"
+                                       defaultContent:@"默认分享内容，没内容时显示"
+                                                image:[ShareSDK imageWithPath:imagePath]
+                                                title:@"ShareSDK"
+                                                  url:@"http://www.sharesdk.cn"
+                                          description:@"这是一条测试信息"
+                                            mediaType:SSPublishContentMediaTypeNews];
+    
+    [ShareSDK showShareActionSheet:nil
+                         shareList:nil
+                           content:publishContent
+                     statusBarTips:YES
+                       authOptions:nil
+                      shareOptions: nil
+                            result:^(ShareType type, SSResponseState state, id<ISSPlatformShareInfo> statusInfo, id<ICMErrorInfo> error, BOOL end) {
+                                if (state == SSResponseStateSuccess)
+                                {
+                                    NSLog(@"分享成功");
+                                }
+                                else if (state == SSResponseStateFail)
+                                {
+                                    NSLog(@"分享失败,错误码:%d,错误描述:%@", [error errorCode], [error errorDescription]);
+                                }
+                            }];
 }
 
 -(void)clickShoutRank:(UITapGestureRecognizer*)sender{
